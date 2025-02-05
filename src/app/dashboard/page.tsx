b5d60d02ab3609c1a81e2baa684b51d7fa02f928
dextermichael <dextermichael.h@gmail.com>
2025-02-06 03:21:47 +0500

    build fix

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,7 +2,8 @@
 import Dogs from '@/components/Dogs';
 import { fetchBreads, fetchDogs } from '@/utils/Function';
 import React from 'react'
-
+// Opt out of static generation
+export const dynamic = 'force-dynamic'; // Force dynamic rendering
 export default async function page() {
     const dogs = await fetchDogs("/dogs/search?size=20&from=0&sort=breed:asc");
     const breads = await fetchBreads();
